162b8da6adadc1b1d9a74cce4eaee56ece609a8c
leasip <leo.assiep@gmail.com>
2023-02-22 23:13:26 +0200

    uudistuksia logon kanssa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 import { ContactsComponent } from './pages/contacts/contacts.component';
 
+
 const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'contacts', component: ContactsComponent }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,59 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainComponent } from './pages/main/main.component';
-import { ContactsComponent } from './pages/contacts/contacts.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
+
+// MDB Modules
+import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
+import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
+import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
+import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
+import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
+import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
+import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
+import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
+import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './shared/navbar/navbar.component';
-import {MatIconModule} from '@angular/material/icon';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { MainComponent } from './pages/main/main.component';
+import { AppRoutingModule } from './app-routing.module';
+
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent,
-    ContactsComponent,
+    NavbarComponent,
     FooterComponent,
-    NavbarComponent
+    ContactsComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MdbAccordionModule,
+    MdbCarouselModule,
+    MdbCheckboxModule,
+    MdbCollapseModule,
+    MdbDropdownModule,
+    MdbFormsModule,
+    MdbModalModule,
+    MdbPopoverModule,
+    MdbRadioModule,
+    MdbRangeModule,
+    MdbRippleModule,
+    MdbScrollspyModule,
+    MdbTabsModule,
+    MdbTooltipModule,
+    MdbValidationModule,
     AppRoutingModule,
-    MatToolbarModule,
-    MatIconModule
-  ],
-  exports: [
-    MatToolbarModule,
-    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]

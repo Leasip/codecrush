bc0338b79f4d5f13bc25467841ecc24e09e5a289
leasip <leo.assiep@gmail.com>
2023-03-05 22:03:24 +0200

    little changes to active tab highlights

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLinkActive } from '@angular/router';
+// import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
-  styles : ['div > a.active {color: red;}']
+  styles : ['div > a.active {color: black;background-color: #00b4ba;box-shadow: 0 0 10px #008cff;}']
 })
 export class NavbarComponent {
 

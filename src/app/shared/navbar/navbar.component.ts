52a66f405139dcabe623b113945feeef4b27aaf3
leasip <leo.assiep@gmail.com>
2023-03-05 21:26:30 +0200

    active tab on navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  styles : ['div > a.active {color: red;}']
 })
 export class NavbarComponent {
 
